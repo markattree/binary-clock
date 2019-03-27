781adfc5d775ccf33c9756e4daeeafaef7bbaa49
Mark Attree <markattree@hotmail.com>
2019-03-27 19:15:38 +0000

    Tidied up the code, moved things into functions

--- static/js/script.js
+++ static/js/script.js
@@ -8,58 +8,54 @@ function updateTime() {
     var binaryMinutes = (decimalMinutes).toString(2).padStart(6, "0");
     var binaryHours = (decimalHours).toString(2).padStart(5, "0");
 
-    var figure = 32;
-    var figure2 = 16;
+    updateBinaryRow(binarySeconds, 32, "s");
+    updateBinaryRow(binaryMinutes, 32, "m");
+    updateBinaryRow(binaryHours, 16, "h");
 
-    for (var index = 0; index < binarySeconds.length; index++) {
-        var elementName = "s" + figure;
-
-        if (binarySeconds.charAt(index) === "1") {
-            document.getElementById(elementName).classList.add("on");
-            document.getElementById(elementName).classList.remove("off");
-        } else {
-            document.getElementById(elementName).classList.add("off");
-            document.getElementById(elementName).classList.remove("on");
-        }
-
-        figure = figure / 2;
-    }
-
-    figure = 32;
-
-    for (var index = 0; index < binaryMinutes.length; index++) {
-        var elementName = "m" + figure;
-
-        if (binaryMinutes.charAt(index) === "1") {
-            document.getElementById(elementName).classList.add("on");
-            document.getElementById(elementName).classList.remove("off");
-        } else {
-            document.getElementById(elementName).classList.add("off");
-            document.getElementById(elementName).classList.remove("on");
-        }
-
-        figure = figure / 2;
-    }
-
-    for (var index = 0; index < binaryHours.length; index++) {
-        var elementName = "h" + figure2;
-
-        if (binaryHours.charAt(index) === "1") {
-            document.getElementById(elementName).classList.add("on");
-            document.getElementById(elementName).classList.remove("off");
-        } else {
-            document.getElementById(elementName).classList.add("off");
-            document.getElementById(elementName).classList.remove("on");
-        }
-
-        figure2 = figure2 / 2;
-    }
-
-    document.getElementById("decimal-hours").innerHTML = decimalHours
-    document.getElementById("decimal-mins").innerHTML = decimalMinutes
-    document.getElementById("decimal-secs").innerHTML = decimalSeconds
+    updateDecimalRow("decimal-hours", decimalHours);
+    updateDecimalRow("decimal-mins", decimalMinutes);
+    updateDecimalRow("decimal-secs", decimalSeconds);
 
     setTimeout(updateTime, 1000);
 }
+
+function updateBinaryRow(binaryNumber, mostSignificantFigure, elementPrefix) {
+    var currentFigure = mostSignificantFigure;
+
+    for (var index = 0; index < binaryNumber.length; index++) {
+        var elementName = getElementName(elementPrefix, currentFigure);
+
+        if (isAOne(binaryNumber.charAt(index))) {
+            turnOnBinaryUnit(elementName);
+        } else {
+            turnOffBinaryUnit(elementName);
+        }
+
+        currentFigure = currentFigure / 2;
+    }
+}
+
+function updateDecimalRow(elementName, decimalValue) {
+    document.getElementById(elementName).innerHTML = decimalValue;
+}
+
+function turnOnBinaryUnit(elementName) {
+    document.getElementById(elementName).classList.add("on");
+    document.getElementById(elementName).classList.remove("off");
+}
+
+function turnOffBinaryUnit(elementName) {
+    document.getElementById(elementName).classList.add("off");
+    document.getElementById(elementName).classList.remove("on");
+}
+
+function isAOne(digit) {
+    return digit === "1";
+}
+
+function getElementName(prefix, suffix) {
+    return prefix + suffix;
+}
+
 
 window.onload = updateTime;
